f78455b989f8e60c64866569fef2be94af85e977
ChaeHyunLim <chaehyun010104@gmail.com>
2026-04-23 16:15:38 +0900

    setting : 프로젝트 생성

---
+++ src/test/java/com/example/springitemservice/SpringItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springitemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
