12634993b73babe6f351de0776c91217a30bec62
ChaeHyunLim <chaehyun010104@gmail.com>
2026-04-23 16:45:44 +0900

    section 8 : 상품 도메인 개발

---
+++ src/main/java/com/example/springitemservice/domain/item/Item.java
@@ -0,0 +1,25 @@
+package com.example.springitemservice.domain.item;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price; // price가 null인 경우도 있다고 고려해서 int가 아닌 Integer
+    private Integer quantity;
+
+    public Item() {
+
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
